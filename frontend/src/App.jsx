67eb99a9f544a3f060cf0b3c3dce8a590a3cba76
AshleyEvonne <ashleyevonne345@gmail.com>
2024-09-11 00:23:07 -0400

    Setting up vite project with tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+          <h1 className="text-3xl font-bold underline bg-red-800 text-center">
+      Hello welcome to sportscards
+    </h1>
+    </>
+  );
+}
+
+export default App;
